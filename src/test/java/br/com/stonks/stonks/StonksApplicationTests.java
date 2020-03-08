08d92e49c4a9e020635cbb41121b43bfc62fa627
LimaLucas96 <l.lima19@hotmail.com>
2020-03-07 21:55:48 -0300

    INIT COMMIT

---
+++ src/test/java/br/com/stonks/stonks/StonksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stonks.stonks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
