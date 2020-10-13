9f5425d9578ec34b0f30059084cb53ecb9cd34ff
LimaLucas96 <l.lima19@hotmail.com>
2020-10-13 19:15:56 -0300

    Modificacoes de layout

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# stonks
-a java base application to monitore and analyze data from stock exchange market.
--- src/main/java/br/com/stonks/stonks/controllers/AuthenticationController.java
+++ src/main/java/br/com/stonks/stonks/controllers/AuthenticationController.java
@@ -4,13 +4,9 @@ import br.com.stonks.stonks.models.Usuario;
 import br.com.stonks.stonks.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
 
 @Controller
 public class AuthenticationController {
@@ -40,7 +36,7 @@ public class AuthenticationController {
         modelAndView.setViewName("home");
 //        return modelAndView;
 
-        return "home";
+        return "dashboard/home";
     }
 
 //    @RequestMapping(value = "/usuario/register", method = RequestMethod.POST)
