2dfd3b4fdb7d619fb4e0317a5f081e3b529e296d
Abmael <abmaeld@gmail.com>
2020-10-26 12:51:28 -0300

    JavaMailSender /relatorio/enviar

--- src/main/java/br/com/stonks/stonks/controllers/CarteiraAtivoController.java
+++ src/main/java/br/com/stonks/stonks/controllers/CarteiraAtivoController.java
@@ -1,6 +1,6 @@
 package br.com.stonks.stonks.controllers;
 
-import br.com.stonks.stonks.exception.EmailInvalidoException;
+import br.com.stonks.stonks.EmailConfig;
 import br.com.stonks.stonks.models.Carteira;
 import br.com.stonks.stonks.models.CarteiraAtivo;
 import br.com.stonks.stonks.models.Operacao;
@@ -9,6 +9,9 @@ import br.com.stonks.stonks.services.CarteiraAtivoService;
 import br.com.stonks.stonks.services.CarteiraService;
 import br.com.stonks.stonks.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -32,6 +35,8 @@ public class CarteiraAtivoController {
 
     @Autowired
     private UsuarioService usuarioService;
+    
+    private EmailConfig emailConfig;
  
 
     @GetMapping(value = "/carteiraativo/relatorio")
@@ -45,6 +50,45 @@ public class CarteiraAtivoController {
 
         model.addAttribute("ativosCarteira", ativos);
         model.addAttribute("usuario", usuario);
+
+        return "dashboard/imprimirRelatorio";
+    }
+    
+    @GetMapping(value = "/carteiraativo/relatorio/enviar")
+    public String enviarRelatorio(Model model){
+
+        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Usuario usuario = usuarioService.usuarioPorEmail(principal.getUsername());
+        Carteira carteira = carteiraService.carteiraByUsuario(usuario);
+
+        List<CarteiraAtivo> ativos = carteiraAtivoService.findByCarteira(carteira.getId());
+        
+        String body = "Ativo / Valor / Quantidade / Data da Transação\n";
+        
+        for (CarteiraAtivo ca : ativos) {
+        	body += ca.getAtivo().getCodigo() 
+        			+ " / " + ca.getValor() 
+        			+ " / " + ca.getQuantidade()
+        			+ " / " + ca.getDataTransacao() + "\n";
+        }
+
+        model.addAttribute("ativosCarteira", ativos);
+        model.addAttribute("usuario", usuario);
+        
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(emailConfig.getHost());
+        mailSender.setPort(emailConfig.getPort());
+        mailSender.setUsername(emailConfig.getUsername());
+        mailSender.setPassword(emailConfig.getPassword());
+        
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        
+        mailMessage.setFrom("admin@stonks.com");
+        mailMessage.setTo(usuario.getEmail());
+        mailMessage.setSubject("Relatório do Stonks");
+        mailMessage.setText(body);
+        
+        mailSender.send(mailMessage);
 
         return "dashboard/imprimirRelatorio";
     }
