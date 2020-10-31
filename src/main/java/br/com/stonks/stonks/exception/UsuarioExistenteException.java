8701419693fd7361bb22e7b642872b63bec38bb6
Lucas <l.lima19@hotmail.com>
2020-10-31 14:20:53 -0300

    Adicionado a tela de registro de usuario e corrigido o bug do dashboard sem carteira.

--- src/main/java/br/com/stonks/stonks/exception/UsuarioExistenteException.java
+++ src/main/java/br/com/stonks/stonks/exception/UsuarioExistenteException.java
@@ -2,5 +2,6 @@ package br.com.stonks.stonks.exception;
 
 public class UsuarioExistenteException extends Exception {
     public UsuarioExistenteException(String mensage) {
+        super(mensage);
     }
 }
